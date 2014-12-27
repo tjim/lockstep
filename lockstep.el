5fe78e94533c4a050d3fce23685040ac754c2e54
Trevor Jim <tjim@mac.com>
2014-12-27 10:41:02 -0800

    Fix for emacs 24.4
    window-state-put has been changing and now does more checks than before,
    breaking lockstep.
    This happened before emacs-24.4 but I have not tracked down the exact
    commit.
    I've removed the additional checks for now.

--- lockstep.el
+++ lockstep.el
@@ -60,11 +60,124 @@
                      (while this-frame-windows
                        (let ((this-frame-window (pop this-frame-windows))
                              (other-frame-window (pop other-frame-windows)))
-                         (window-state-put (window-state-get this-frame-window) other-frame-window)
+                         (lockstep-window-state-put (window-state-get this-frame-window) other-frame-window)
                          (when master-frame
                            ; if called from turn-on-lockstep, synchronize point as well
                            (set-window-point other-frame-window (window-point this-frame-window))))))))
       (add-hook 'window-configuration-change-hook 'lockstep-frame))))
+
+; modified from emacs 24.4
+(defun lockstep-window-state-put (state &optional window ignore)
+  "Put window state STATE into WINDOW.
+STATE should be the state of a window returned by an earlier
+invocation of `window-state-get'.  Optional argument WINDOW must
+specify a valid window and defaults to the selected one.  If
+WINDOW is not live, replace WINDOW by a live one before putting
+STATE into it.
+
+Optional argument IGNORE non-nil means ignore minimum window
+sizes and fixed size restrictions.  IGNORE equal `safe' means
+windows can get as small as `window-safe-min-height' and
+`window-safe-min-width'."
+  (setq window-state-put-stale-windows nil)
+  (setq window (window-normalize-window window))
+
+  ;; When WINDOW is internal, reduce it to a live one to put STATE into,
+  ;; see Bug#16793.
+  (unless (window-live-p window)
+    (let ((root (frame-root-window window)))
+      (if (eq window root)
+	  (setq window (frame-first-window root))
+	(setq root window)
+	(setq window (catch 'live
+		       (walk-window-subtree
+			(lambda (window)
+			  (when (window-live-p window)
+			    (throw 'live window)))
+			root))))
+      (delete-other-windows-internal window root)))
+
+  (let* ((frame (window-frame window))
+	 (head (car state))
+	 ;; We check here (1) whether the total sizes of root window of
+	 ;; STATE and that of WINDOW are equal so we can avoid
+	 ;; calculating new sizes, and (2) if we do have to resize
+	 ;; whether we can do so without violating size restrictions.
+	 (pixelwise (and (cdr (assq 'pixel-width state))
+			 (cdr (assq 'pixel-height state))))
+	 (totals (or (and pixelwise
+			  (= (window-pixel-width window)
+			     (cdr (assq 'pixel-width state)))
+			  (= (window-pixel-height window)
+			     (cdr (assq 'pixel-height state))))
+		     (and (= (window-total-width window)
+			     (cdr (assq 'total-width state)))
+			  (= (window-total-height window)
+			     (cdr (assq 'total-height state))))))
+	 (min-height (cdr (assq
+			   (if pixelwise 'min-pixel-height 'min-height)
+			   head)))
+	 (min-width (cdr (assq
+			  (if pixelwise 'min-pixel-width 'min-weight)
+			  head))))
+    (if (and nil
+             (not totals)
+	     (or (> min-height (window-size window nil pixelwise))
+		 (> min-width (window-size window t pixelwise)))
+	     (or (not ignore)
+		 (and (setq min-height
+			    (cdr (assq
+				  (if pixelwise
+				      'min-pixel-height-ignore
+				    'min-height-ignore)
+				  head)))
+		      (setq min-width
+			    (cdr (assq
+				  (if pixelwise
+				      'min-pixel-width-ignore
+				    'min-width-ignore)
+				  head)))
+		      (or (> min-height
+			     (window-size window nil pixelwise))
+			  (> min-width
+			     (window-size window t pixelwise)))
+		      (or (not (eq ignore 'safe))
+			  (and (setq min-height
+				     (cdr (assq
+					   (if pixelwise
+					       'min-pixel-height-safe
+					     'min-height-safe)
+					   head)))
+			       (setq min-width
+				     (cdr (assq
+					   (if pixelwise
+					       'min-pixel-width-safe
+					     'min-width-safe)
+					   head)))
+			       (or (> min-height
+				      (window-size window nil pixelwise))
+				   (> min-width
+				      (window-size window t pixelwise))))))))
+	;; The check above might not catch all errors due to rounding
+	;; issues - so IGNORE equal 'safe might not always produce the
+	;; minimum possible state.  But such configurations hardly make
+	;; sense anyway.
+	(error "Window %s too small to accommodate state" window)
+      (setq state (cdr state))
+      (setq window-state-put-list nil)
+      ;; Work on the windows of a temporary buffer to make sure that
+      ;; splitting proceeds regardless of any buffer local values of
+      ;; `window-size-fixed'.  Release that buffer after the buffers of
+      ;; all live windows have been set by `window--state-put-2'.
+      (with-temp-buffer
+	(set-window-buffer window (current-buffer))
+	(window--state-put-1 state window nil totals pixelwise)
+	(window--state-put-2 ignore pixelwise))
+      (while window-state-put-stale-windows
+	(let ((window (pop window-state-put-stale-windows)))
+	  (when (eq (window-deletable-p window) t)
+	    (delete-window window))))
+      (window--check frame))))
 
 (defun lockstep-point ()
   "Synchronize point in all windows in other lockstep frames visiting this buffer."
